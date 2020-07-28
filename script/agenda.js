25bf122c07b24948f148d9325c54041becc53339
nicolas98r <nico98.snrm@gmail.com>
2020-07-28 10:33:21 -0500

    Versión Funcional (bastante mediocre)

--- script/agenda.js
+++ script/agenda.js
@@ -1,6 +1,25 @@
-function agregarValores() {
+var contactos = new Array();
+function contacto() {
+    this.nombre = "";
+    this.apellido = "";
+    this.correo = "";
+    this.telefono = "";
+}
+
+function agregarContacto() {
+    objCon = new contacto();
+    objCon.nombre = document.getElementById("nombre").value;
+    objCon.apellido = document.getElementById("apellido").value;
+    objCon.correo = document.getElementById("correo").value;
+    objCon.telefono = document.getElementById("telefono").value;
     document.getElementById("nombre").value = "";
     document.getElementById("apellido").value = "";
     document.getElementById("correo").value = "";
-    document.getElementsById("telefono").value = "";
+    document.getElementById("telefono").value = "";
+    alert("Contacto añadido");
+    var registro = "<tr><td>" + objCon.nombre + "</td><td>" + objCon.apellido + "</td><td>" + objCon.correo + "</td><td>" + objCon.telefono + "</td></tr>";
+    var fila = document.createElement("TR");
+    fila.innerHTML = registro;
+    document.getElementById("cuerpoTabla").appendChild(fila);
+    contactos[contactos.length] = objCon;
 }
